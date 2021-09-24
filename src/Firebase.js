238f4d34f681597b0252a33a59821f4a1ed32ba0
kritika243 <24kritikasharma24@gmail.com>
2021-09-24 20:39:48 +0530

    🛠 fixed api key error

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/compat/app'
 import { initializeApp } from 'firebase/app'
+import config from './config'
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -10,12 +11,12 @@ import {
 } from 'firebase/auth'
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: config.REACT_APP_FIREBASE_API_KEY,
+  authDomain: config.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: config.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: config.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.REACT_APP_FIREBASE_APP_ID,
 })
 
 export const auth = getAuth()
