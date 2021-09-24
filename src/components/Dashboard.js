614711ae85da9dbbaf5fa139589260df59eb4706
kritika243 <24kritikasharma24@gmail.com>
2021-09-24 23:45:10 +0530

    dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Card, Alert } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 function Dashboard() {
-  return <div>Dashboard</div>
+  const [error, setError] = useState('')
+  const { currentUser } = useAuth()
+
+  function handleLogout() {}
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Your Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <strong>Email: </strong> {currentUser.email}
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update your profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2 '>
+        <Button variant='link' onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
