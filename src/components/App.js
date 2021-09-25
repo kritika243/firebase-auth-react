82aef8f3578b7328022b88bcfce5778efa684808
kritika243 <24kritikasharma24@gmail.com>
2021-09-25 12:49:36 +0530

    reset password page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { AuthProvider } from '../contexts/AuthContext'
 import Signup from './Signup'
 import Dashboard from './Dashboard'
 import Login from './Login'
+import ForgotPassword from './ForgotPassword'
 import PrivateRoute from './PrivateRoute'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -19,6 +20,7 @@ function App() {
               <PrivateRoute exact path='/' component={Dashboard} />
               <Route path='/signup' component={Signup} />
               <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -3,9 +3,8 @@ import { Link, useHistory } from 'react-router-dom'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 
-function Login() {
+function ForgotPassword() {
   const emailRef = useRef()
-  const passwordRef = useRef()
   const { login } = useAuth()
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
@@ -17,7 +16,7 @@ function Login() {
     try {
       setError('')
       setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
+      // await login(emailRef.current.value, passwordRef.current.value)
       history.push('/')
     } catch {
       setError('Failed to login')
@@ -29,7 +28,7 @@ function Login() {
     <>
       <Card>
         <Card.Body>
-          <h2 className='text-center mb-4'>Log In</h2>
+          <h2 className='text-center mb-4'>Reset Your Password</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id='email' className='mb-3'>
@@ -37,17 +36,12 @@ function Login() {
               <Form.Control type='email' ref={emailRef} required />
             </Form.Group>
 
-            <Form.Group id='password' className='mb-4'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
-            </Form.Group>
-
             <Button disabled={loading} className='w-100' type='submit'>
-              Log In
+              Reset Password
             </Button>
           </Form>
           <div className='w-100 text-center mt-3 '>
-            <Link to='/forgot-password'>Forgot Password ?</Link>
+            <Link to='/login'>Login</Link>
           </div>
         </Card.Body>
       </Card>
@@ -58,4 +52,4 @@ function Login() {
   )
 }
 
-export default Login
+export default ForgotPassword
